bbc05e6b2486d49ca27817f4ef283719535785e2
杨小胖 <pike.yang@livestar.com>
2018-02-27 11:38:32 +0800

    ADD: test6_3，test6_4

--- LayerTest/ViewController.swift
+++ LayerTest/ViewController.swift
@@ -29,8 +29,13 @@ class ViewController: UIViewController {
 //        test6_1()
 //        test6_1_1()
 //        test6_2_1()
-        test6_2_2()
+//        test6_2_2()
+//        test6_3()
+//        test6_4_1()
+        test6_4_2()
     }
+    
+    // MARK: - test2
     
     func test2_1() {
         let image = UIImage(named: "setting_record_novideo")
@@ -54,11 +59,15 @@ class ViewController: UIViewController {
         bluelayer.display()
     }
     
+    // MARK: - test3
+    
     func test3_4() {
         blueLayer.frame = CGRect(x: 50, y: 50, width: 100, height: 100)
         blueLayer.backgroundColor = UIColor.blue.cgColor
         layerView.layer.addSublayer(blueLayer)
     }
+    
+    // MARK: - test4
     
     func test4_3() {
         let circlePath = CGPath(ellipseIn: layerView.bounds, transform: nil)
@@ -102,6 +111,8 @@ class ViewController: UIViewController {
         return button
     }
     
+    // MARK: - test5
+    
     func test5_1() {
 //        let transform = CGAffineTransform.identity
         let scaleTransform = CGAffineTransform.init(scaleX: 0.5, y: 0.5)
@@ -118,6 +129,8 @@ class ViewController: UIViewController {
         
         layerView.layer.transform = transform
     }
+    
+    // MARK: - test6
     
     func test6_1() {
         let path = UIBezierPath()
@@ -193,6 +206,113 @@ class ViewController: UIViewController {
         layerLabel.setFont(font: font)
     }
     
+    func test6_3() {
+        //set up the perspective transform
+        var pt = CATransform3DIdentity
+        pt.m34 = -1.0 / 500.0
+        layerView.layer.sublayerTransform = pt
+        
+        //set up the transform for cube 1 and add it
+        let c1t = CATransform3DMakeTranslation(-100, 0, 0)
+        let cube1 = cubeWithTransform(c1t)
+        layerView.layer.addSublayer(cube1)
+        
+        //set up the transform for cube 2 and add it
+        var c2t = CATransform3DIdentity
+        c2t = CATransform3DTranslate(c2t, 100, 0, 0);
+        c2t = CATransform3DRotate(c2t, CGFloat(-Double.pi / 4), 1, 0, 0);
+        c2t = CATransform3DRotate(c2t, CGFloat(-Double.pi / 4), 0, 1, 0);
+        let cube2 = cubeWithTransform(c2t)
+        layerView.layer.addSublayer(cube2)
+        
+    }
+    
+    func faceWithTransform(_ transform: CATransform3D) -> CALayer {
+        //create cube face layer
+        let face = CALayer()
+        face.frame = CGRect(x: -50, y: -50, width: 100, height: 100)
+        
+        //apply a random color
+        face.backgroundColor = UIColor.randomColor.cgColor
+        
+        //apply the transform and return
+        face.transform = transform
+        return face
+    }
+    
+    func cubeWithTransform(_ transform: CATransform3D) -> CALayer{
+        //create cube layer
+        let cube = CATransformLayer()
+        
+        //add cube face 1
+        var ct = CATransform3DMakeTranslation(0, 0, 50)
+        cube.addSublayer(faceWithTransform(ct))
+        
+        //add cube face 2
+        ct = CATransform3DMakeTranslation(50, 0, 0)
+        ct = CATransform3DRotate(ct, CGFloat(Double.pi * 0.5), 0, 1, 0)
+        cube.addSublayer(faceWithTransform(ct))
+        
+        //add cube face 3
+        ct = CATransform3DMakeTranslation(0, -50, 0)
+        ct = CATransform3DRotate(ct, CGFloat(Double.pi * 0.5), 1, 0, 0)
+        cube.addSublayer(faceWithTransform(ct))
+        
+        //add cube face 4
+        ct = CATransform3DMakeTranslation(0, 50, 0)
+        ct = CATransform3DRotate(ct, CGFloat(-Double.pi * 0.5), 1, 0, 0)
+        cube.addSublayer(faceWithTransform(ct))
+        
+        //add cube face 5
+        ct = CATransform3DMakeTranslation(-50, 0, 0)
+        ct = CATransform3DRotate(ct, CGFloat(-Double.pi * 0.5), 0, 1, 0)
+        cube.addSublayer(faceWithTransform(ct))
+        
+        //add cube face 6
+        ct = CATransform3DMakeTranslation(0, 0, -50)
+        ct = CATransform3DRotate(ct, CGFloat(Double.pi), 0, 1, 0)
+        cube.addSublayer(faceWithTransform(ct))
+        
+        //center the cube layer within the container
+        let containerSize = layerView.bounds.size
+        cube.position = CGPoint(x: containerSize.width * 0.5, y: containerSize.height * 0.5)
+        
+        //apply the transform and return
+        cube.transform = transform
+        return cube
+    }
+    
+    func test6_4_1() {
+        //create gradient layer and add it to our container view
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = layerView.bounds
+        layerView.layer.addSublayer(gradientLayer)
+        
+        //set gradient colors
+        gradientLayer.colors = [UIColor.red.cgColor, UIColor.blue.cgColor]
+        
+        //set gradient start and end points
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+    }
+    
+    func test6_4_2() {
+        //create gradient layer and add it to our container view
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = layerView.bounds
+        layerView.layer.addSublayer(gradientLayer)
+        
+        //set gradient colors
+        gradientLayer.colors = [UIColor.red.cgColor, UIColor.yellow.cgColor, UIColor.green.cgColor]
+        
+        //set locations
+        gradientLayer.locations = [0.0, 0.25, 0.5];
+        
+        //set gradient start and end points
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+    }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         /// test3_4
 //        let point = touches.first!.location(in: view)
@@ -222,5 +342,16 @@ extension ViewController: CALayerDelegate {
         ctx.setLineWidth(10)
         ctx.setStrokeColor(UIColor.red.cgColor)
         ctx.strokeEllipse(in: layer.bounds)
+    }
+}
+
+extension UIColor {
+    class var randomColor:UIColor{
+        get {
+            let red = CGFloat(arc4random() % 256) / 255.0
+            let green = CGFloat(arc4random() % 256) / 255.0
+            let blue = CGFloat(arc4random() % 256) / 255.0
+            return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+        }
     }
 }
