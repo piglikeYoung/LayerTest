a29ff7fa4267e3cafcaae12a1a0c50c2e60d33f1
piglikeyoung <316046510@qq.com>
2018-02-28 23:24:04 +0800

    ADD: test7_4

--- LayerTest/ViewController2.swift
+++ LayerTest/ViewController2.swift
@@ -20,10 +20,13 @@ class ViewController2: UIViewController {
         return layer
     }()
     
+    var colorLayer2: CALayer?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        test7_4()
     }
     
     @IBAction func changeColorClicked(_ sender: Any) {
@@ -31,7 +34,7 @@ class ViewController2: UIViewController {
 //        test7_2()
 //        test7_3_1()
 //        test7_3_2()
-        test7_3_3()
+//        test7_3_3()
     }
     
     func test7_1() {
@@ -78,5 +81,31 @@ class ViewController2: UIViewController {
         colorLayer.actions = ["backgroundColor": transition]
         
         colorLayer.backgroundColor = UIColor.randomColor.cgColor
+    }
+    
+    func test7_4() {
+        let layer = CALayer()
+        layer.frame = CGRect(x: 50, y: 50, width: 100, height: 100)
+        layer.backgroundColor = UIColor.blue.cgColor
+        view.layer.addSublayer(layer)
+        colorLayer2 = layer
+    }
+    
+    // test7_4
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //get the touch point
+        let point = touches.first?.location(in: view)
+        
+        //check if we've tapped the moving layer
+        if ((colorLayer2?.presentation()?.hitTest(point!)) != nil) {
+            //randomize the layer background color
+            colorLayer2?.backgroundColor = UIColor.randomColor.cgColor
+        } else {
+            //otherwise (slowly) move the layer to new position
+            CATransaction.begin()
+            CATransaction.setAnimationDuration(4)
+            colorLayer2?.position = point!
+            CATransaction.commit()
+        }
     }
 }
